6398da76af8150c6d67dd400c18fcf65f3ef0ca1
kylewetton <kylewetton@me.com>
2019-10-25 12:49:16 +1300

    Minify and purge

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,7 +1,9 @@
 const Path = require("path");
 const Webpack = require("webpack");
 const merge = require("webpack-merge");
+const TerserJSPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
@@ -12,6 +14,9 @@ module.exports = merge(common, {
   output: {
     filename: "js/[name].[chunkhash:8].js",
     chunkFilename: "js/[name].[chunkhash:8].chunk.js"
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
   },
   plugins: [
     new Webpack.DefinePlugin({
